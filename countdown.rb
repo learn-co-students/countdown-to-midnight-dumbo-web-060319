a29d32d817c6a975ffaa4cc3dfc879840d2d312a
Alexander Schelchere <ascellie@gmail.com>
2019-05-10 10:23:14 -0400

    Countdowns!

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,33 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  yay = "HAPPY NEW YEAR!"
+  # While loop
+    while int > 0 
+       # Print
+      puts "#{int} SECOND(S)!"
+      # decrement
+      int -= 1 
+    end
+
+  # return
+  yay
+end
+
+def countdown_with_sleep(int)
+  sleep(5)
+  yay = "HAPPY NEW YEAR!"
+  # While loop
+  while int >= 0
+    # Print
+    puts "#{int} SECOND(S)!".chomp
+    # decrement
+    int -= 1
+    # wait a second
+    sleep(1)
+  end
+  # print
+  puts yay
+  # return
+  yay
 end

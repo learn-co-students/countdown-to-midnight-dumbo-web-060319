f076f9d5caa9cfdc7fe70541f89fb1bd8ae9ac5f
Richard Li <richardli125@gmail.com>
2019-05-16 00:46:55 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(interger)
+  while interger > 0
+    puts "#{interger} SECOND(S)!"
+    interger -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(interger)
+  sleep(interger)
 end

155a3b11dffc6d61ac49535b3c153cc2f98fb36c
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-05-23 16:55:51 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number-=
+    end 
+    return "HAPPY NEW YEAR!" if  number == 0
 end
+#countdown 

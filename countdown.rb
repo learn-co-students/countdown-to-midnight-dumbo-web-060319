8e461d64b786d975b084e1b5ec705db1ff5be718
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-25 05:49:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+  while arg > 0 
+    puts "#{arg} SECOND(S)!"
+    arg -= 1 
+  end 
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(arg)
+  while arg > 0
+    puts "#{arg} SECOND(S)!"
+    arg -= 1 
+    sleep(1)
+  end 
+  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
+end 

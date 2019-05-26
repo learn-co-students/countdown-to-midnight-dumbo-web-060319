ed080e41e7aefe9a5cb7d50d56e2a7812b30effa
Olga Rosas <olga.rosas@gmail.com>
2019-05-26 13:41:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(int)
+x = 10
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num_secs)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
